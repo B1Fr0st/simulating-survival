7b415ddc30c33a402287c406cec102e15900d934
BiFr0st <95887189+B1Fr0st@users.noreply.github.com>
2021-12-10 19:52:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class Survivor():
+  def __init__(self,hunger,water,health,isSick):
+    self.hunger = hunger
+    self.water = water
+    self.health = health
+    self.isSick = isSick
+    self.sickDays = 0
+  def passDay():
+    if(self.isSick && self.sickDays):
