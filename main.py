ec22c9d5b029125fa61e1e57eccc802bdaea0c09
BiFr0st <95887189+B1Fr0st@users.noreply.github.com>
2021-12-13 16:55:10 +0000

    Added data processing,creating survivors, and data pushing.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 survivors = []
 errorlog = []
 def constrain(value,min,max):
@@ -9,13 +11,15 @@ def constrain(value,min,max):
     val = value
   return val
 class Survivor():
-  def __init__(self,hunger,water,health,isSick,survI):
+  def __init__(self,hunger,water,health,isSick,survI,waterProb,hungerProb):
     self.hunger = hunger
     self.water = water
     self.health = health
     self.isSick = isSick
     self.sickDays = 0
     self.survI = survI
+    self.waterProb = waterProb
+    self.hungerProb = hungerProb
   def checkDeath(self):
     if self.health <= 0:
       try:
@@ -24,6 +28,11 @@ class Survivor():
         errorlog.append("Error occured in GarbageCollection:"+e)
   def passDay(self):
     self.checkDeath()
+    if random.randint(0,100) < self.waterProb:
+      self.water += 15
+    if random.randint(0,100) < self.hungerProb:
+      self.hunger += 25
+    
     if self.isSick:
       if self.sickDays < 3:
         self.health -=5
@@ -49,11 +58,36 @@ class Survivor():
     self.health = constrain(self.health,0,100)
   
 
-survivors.append(Survivor(100,100,100,False,0))
-print(survivors)
-while True:
+
+def processAvg(data):
+    sumTotal = 0
+    for point in data:
+        sumTotal += point
+    return sumTotal/len(data)
+def processMax(data):
+    curMax = 0
+    for point in data:
+        if point > curMax:
+            curMax = point
+    return curMax
+def processMin(data):
+    curMin = processMax(data)
+    for point in data:
+        if point < curMin:
+            curMin = point
+    return curMin
+healthdata = []
+waterdata = []
+hungerdata = []
+survivors.append(Survivor(100,100,100,False,0,5,5))#10 points, dedicated to either water (1) or hunger (2)
+
+
+while len(survivors) > 0:
   for survi in survivors:
-    print(survi.health)
-    print(survivors)
     survi.passDay()
-    print(survi.hunger)
+    healthdata.append([survi.health,survi.waterProb,survi.hungerProb,survi.survI])
+    waterdata.append([survi.water,survi.waterProb,survi.hungerProb,survi.survI])
+    hungerdata.append([survi.hunger,survi.waterProb,survi.hungerProb,survi.survI])
+    print("\n\nCurrent Hunger:%i\nCurrent Water:%i\nCurrent Health:%i" % (survi.hunger,survi.water,survi.health))
+
+
